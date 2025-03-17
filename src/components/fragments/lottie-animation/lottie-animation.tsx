06b2df3ee078d085eed02dc212e9dda68a24d51a
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2025-03-17 18:44:07 +0700

    Add notes on lottie animation import

--- src/components/fragments/lottie-animation/lottie-animation.tsx
+++ src/components/fragments/lottie-animation/lottie-animation.tsx
@@ -1,5 +1,8 @@
 "use client";
 import Lottie from "lottie-react";
+// import animationData from "./JB2G_Lottie.json";
+// * Use above import when run at local server
+// * This is an issue with deployment on Vercel
 import * as animationData from "./JB2G_Lottie.json";
 
 export function LottieAnimation() {
